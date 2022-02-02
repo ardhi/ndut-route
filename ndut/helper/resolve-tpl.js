a968235928722d15c0d53d1d32d6566426a00c1e
Ardhi Lukianto <ardhi@lukianto.com>
2022-02-02 20:16:11 +0700

    Refactor

--- ndut/helper/resolve-tpl.js
+++ ndut/helper/resolve-tpl.js
@@ -1,31 +1,20 @@
 const path = require('path')
 
-module.exports = function (name, from = 'ndutRoute', defTheme = 'default') {
+module.exports = function (name) {
   const { _, fs, getConfig, getNdutConfig } = this.ndut.helper
-  let [ ns, file, theme ] = name.split(':')
+  let [ ns, file ] = name.split(':')
   if (_.isEmpty(file)) {
     file = ns
     ns = 'app'
   }
-  theme = theme || defTheme
   const config = getConfig()
   const cfg = getNdutConfig(ns)
   if (!cfg) throw new Error(`Unknwon namespace '${ns}'`)
   if (file[0] === '.') throw new Error('Relative path is unsupported')
   let ext = path.extname(file)
   if (_.isEmpty(ext)) ext = '.html'
-  let newFile = null
-  if (theme !== 'default') {
-    const tcfg = getNdutConfig(theme)
-    if (tcfg) {
-      newFile = `${tcfg.dir}/${from}/theme/${cfg.alias}/${_.trimStart(file, '/')}${ext}`
-      if (!fs.existsSync(newFile)) newFile = null
-    }
-  }
-  if (!newFile) newFile = `${cfg.dir}/${from}/template/${_.trimStart(file, '/')}${ext}`
-  /*
-  const override = `${config.dir.base}/${from}/template/override/${ns}/${_.trimStart(file, '/')}${ext}`
+  let newFile = `${cfg.dir}/ndutRoute/template/${_.trimStart(file, '/')}${ext}`
+  const override = `${config.dir.base}/ndutRoute/template/override/${ns}/${_.trimStart(file, '/')}${ext}`
   if (fs.existsSync(override)) newFile = override
-  */
   return newFile
 }
