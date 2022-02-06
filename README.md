28007fbe177981e0557f4d1f70b80a41002ac102
Ardhi Lukianto <ardhi@lukianto.com>
2022-02-06 09:47:47 +0700

    Level based hooks

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ndut-route
 Route for Ndut Framework
+
+on-request levels:
+
+- session
+- i18n
+- site
+- auth
+- role
--- ndut/helper/prep-interception.js
+++ ndut/helper/prep-interception.js
@@ -12,14 +12,8 @@ module.exports = async function (scope, name, notFoundMsg) {
     const interceptor = `${cfg.dir}${dirPrefix}/interceptor.js`
     if (fs.existsSync(interceptor)) {
       const mod = require(interceptor)
-      await mod.call(scope, cfg)
+      await mod.call(scope, name, notFoundMsg, cfg)
     }
-  }
-  scope.addHook('onRequest', async (request, reply) => {
-    if (!request.routerPath) throw scope.Boom.notFound(notFoundMsg)
-  })
-  for (const n of config.nduts) {
-    const cfg = getNdutConfig(n)
     for (const d of decorators) {
       const file = `${cfg.dir}${dirPrefix}/decorator/${d}.js`
       if (fs.existsSync(file)) {
@@ -33,9 +27,42 @@ module.exports = async function (scope, name, notFoundMsg) {
     hookFiles = _.concat(hookFiles, await fastGlob(`${cfg.dir}${dirPrefix}/hook/*.js`))
   }
   if (hookFiles.length > 0) {
+    const hooks = {}
     for (const f of hookFiles) {
       const method = _.camelCase(path.basename(f, '.js'))
-      scope.addHook(method, require(f))
+      let mod = require(f)
+      if (_.isFunction(mod)) {
+        if (mod.length === 3) mod = await mod.call(scope, name, notFoundMsg, {})
+        else mod = { level: 99, handler: mod }
+      }
+      if (!mod || !mod.handler) continue
+      if (!mod.level) mod.level = 99
+      if (!mod.name) {
+        let name
+        for (const n of config.nduts) {
+          const cfg = getNdutConfig(n)
+          if (f.startsWith(cfg.dir)) {
+            name = cfg.alias
+            continue
+          }
+        }
+        mod.name = name
+      }
+      if (!hooks[method]) hooks[method] = []
+      hooks[method].push(mod)
     }
+    (hooks || {}).onRequest.push({
+      level: 30,
+      name: 'notFound',
+      handler: async function (request, reply) {
+        if (!request.routerPath) throw scope.Boom.notFound(notFoundMsg)
+      }
+    })
+    _.forOwn(hooks, (v, k) => {
+      v = _.orderBy(v, ['level'], ['asc'])
+      for (const item of v) {
+        scope.addHook(k, item.handler)
+      }
+    })
   }
 }
