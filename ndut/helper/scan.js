1a89c80732bbfd43a56c2ca0db291ab03ded221b
Ardhi Lukianto <ardhi@lukianto.com>
2022-02-16 20:44:22 +0700

    Change app base dir

--- ndut/helper/scan.js
+++ ndut/helper/scan.js
@@ -2,25 +2,16 @@ const path = require('path')
 const verbs = ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'CUSTOM']
 
 module.exports = async function (dir = '', options = {}) {
-  const { _, fastGlob, aneka } = this.ndut.helper
-  const { pathResolve } = aneka
-  let baseDir
-  if (!options.root) options.root = 'cwd'
+  const { _, fastGlob } = this.ndut.helper
   if (!options.prefix) options.prefix = '/'
   if (options.prefix[0] !== '/') options.prefix = '/' + options.prefix
   if (_.isEmpty(dir)) throw new Error('Directory to scan is not provided')
-  if (path.isAbsolute(dir)) baseDir = dir
-  else if (options.root === 'cwd') baseDir = this.config.dir.base + '/' + dir
-  else if (options.root === 'dataDir') baseDir = this.config.dir.data + '/' + dir
-  // else if (options.root === 'dirname') baseDir = path.join(__dirname, dir)
-  else baseDir = dir
-  baseDir = pathResolve(baseDir)
-  const files = await fastGlob(`${baseDir}/**/*.js`)
+  const files = await fastGlob(`${dir}/**/*.js`)
   const result = []
   const paths = {}
   for (const file of files) {
     let url = file
-      .replace(baseDir, '')
+      .replace(dir, '')
       .replace(/@@/g, '\t')
       .replace(/@/g, ':')
       .replace(/\t/g, '@')
